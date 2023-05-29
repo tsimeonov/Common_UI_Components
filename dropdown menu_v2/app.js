cc3b0f78b7c51b148de0175ff41fe789c4b748bf
tsimeonov <tsimeonov12@gmail.com>
2023-05-29 17:46:04 +0200

    changes

--- dropdown menu_v2/app.js
+++ dropdown menu_v2/app.js
@@ -3,18 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
   let dropdownMenus = document.querySelectorAll(".dropdown-menu");
 
   function closeAllDropdownMenus() {
-    dropdownMenus.forEach(function (menu) {
-      menu.style.display = "none";
+    dropdownMenus.forEach(function (dropdownMenu) {
+      dropdownMenu.style.display = "none";
+      dropdownMenu.style.opacity = "0";
+      dropdownMenu.style.height = "0";
+      dropdownMenu.style.transition = "opacity 2s ease-in-out";
+      dropdownMenu.style.outline = "1px solid red";
     });
   }
 
   items.forEach(function (item) {
     item.addEventListener("mouseover", function (e) {
       e.preventDefault();
+      closeAllDropdownMenus();
       let target = this.getAttribute("data-target");
       let dropdownMenu = document.getElementById(target);
-      closeAllDropdownMenus();
       dropdownMenu.style.display = "block";
+      dropdownMenu.style.opacity = "1";
+      dropdownMenu.style.height = "auto";
+      dropdownMenu.style.outline = "1px solid blue";
     });
   });
 
