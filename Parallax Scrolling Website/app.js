471d621025317b2f05bccd05df82ab65bb10c19f
tsimeonov <tsimeonov12@gmail.com>
2024-10-05 16:38:22 +0200

    target the elements

---
+++ Parallax Scrolling Website/app.js
@@ -0,0 +1,4 @@
+let pyramid = document.getElementById("pyramid");
+let text = document.getElementById("text");
+let stone = document.getElementById("stone");
+let man = document.getElementById("man");
