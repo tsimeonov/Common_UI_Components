52f14ef9031cab876acac6ed33b52ab3cec59178
tsimeonov <tsimeonov12@gmail.com>
2024-10-05 16:41:57 +0200

    add the scroll event

--- Parallax Scrolling Website/app.js
+++ Parallax Scrolling Website/app.js
@@ -2,3 +2,8 @@ let pyramid = document.getElementById("pyramid");
 let text = document.getElementById("text");
 let stone = document.getElementById("stone");
 let man = document.getElementById("man");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  pyramid.style.right = value * 0.25 + "px";
+});
