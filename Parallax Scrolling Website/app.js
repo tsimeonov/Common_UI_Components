35e5a14b231f32042af7f3ad777e3166657993b9
tsimeonov <tsimeonov12@gmail.com>
2024-10-05 16:44:50 +0200

    refactor the code

--- Parallax Scrolling Website/app.js
+++ Parallax Scrolling Website/app.js
@@ -6,6 +6,7 @@ let man = document.getElementById("man");
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
   pyramid.style.right = value * 0.25 + "px";
-  text.style.right = value * 2 + "px";
+  text.style.left = value * 2 + "px";
   stone.style.right = value * 1 + "px";
+  man.style.left = value * 0.5 + "px";
 });
