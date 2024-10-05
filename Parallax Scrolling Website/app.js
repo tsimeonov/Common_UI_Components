3be9807215ba6d275f2fc10bd41fefe549fbd11b
tsimeonov <tsimeonov12@gmail.com>
2024-10-05 16:43:17 +0200

    move the stone

--- Parallax Scrolling Website/app.js
+++ Parallax Scrolling Website/app.js
@@ -7,4 +7,5 @@ window.addEventListener("scroll", function () {
   let value = window.scrollY;
   pyramid.style.right = value * 0.25 + "px";
   text.style.right = value * 2 + "px";
+  stone.style.right = value * 1 + "px";
 });
